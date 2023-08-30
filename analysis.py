8b11f702d7c5dfe2750ef51144b40693813c1b73
Dan Jones <dan.j195@outlook.com>
2023-08-30 11:13:01 +0100

    Added comments and more analysis functions

--- analysis.py
+++ analysis.py
@@ -1,13 +1,18 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Load RoBERTa sentiment analysis model
 MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
+# Class for conducting sentiment analysis
 
-class sentiment_analysis:
+
+class SentimentAnalysis:
     def analyse(self, df):
         print("This may take a while...")
 
@@ -32,7 +37,75 @@ class sentiment_analysis:
 
         return df
 
+# Class for displaying sentiment analysis results
 
-class analysis_displayer:
-    def display(self):
-        return
+
+class AnalysisDisplayer:
+    def display(self, df):
+        # Find the most positive comment
+        most_positive_index = df["Positive"].idxmax()
+        most_positive_comment = df.loc[most_positive_index, "Comment"]
+        most_positive_score = df.loc[most_positive_index, "Positive"]
+
+        print("Most Positive Comment:")
+        print("Comment:", most_positive_comment)
+        print("Positive Score:", most_positive_score)
+        print()
+
+        # Find the most negative comment
+        most_negative_index = df["Negative"].idxmax()
+        most_negative_comment = df.loc[most_negative_index, "Comment"]
+        most_negative_score = df.loc[most_negative_index, "Negative"]
+
+        print("Most Negative Comment:")
+        print("Comment:", most_negative_comment)
+        print("Negative Score:", most_negative_score)
+        print()
+
+        # Calculate the average positive score
+        average_positive_score = df["Positive"].mean()
+        print("Average Positive Score:", average_positive_score)
+
+    def display_positive_graph(self, df):
+        # Calculate intervals for positive scores
+        num_intervals = 10
+        interval_size = 1.0 / num_intervals
+        interval_ranges = [(i * interval_size, (i + 1) * interval_size)
+                           for i in range(num_intervals)]
+
+        # Calculate the count of comments in each positive interval
+        interval_counts = [((df["Positive"] >= lower) & (
+            df["Positive"] < upper)).sum() for lower, upper in interval_ranges]
+
+        # Create a bar chart for positive scores distribution
+        plt.bar(range(num_intervals), interval_counts, tick_label=[
+                f"{lower:.1f}-{upper:.1f}" for lower, upper in interval_ranges])
+        plt.xlabel("Positive Score Intervals")
+        plt.ylabel("Number of Comments")
+        plt.title("Distribution of Positive Scores")
+        plt.xticks(rotation=45, ha="right")
+
+        plt.tight_layout()
+        plt.show()
+
+    def display_negative_graph(self, df):
+        # Calculate intervals for negative scores
+        num_intervals = 10
+        interval_size = 1.0 / num_intervals
+        interval_ranges = [(i * interval_size, (i + 1) * interval_size)
+                           for i in range(num_intervals)]
+
+        # Calculate the count of comments in each negative interval
+        interval_counts = [((df["Negative"] >= lower) & (
+            df["Negative"] < upper)).sum() for lower, upper in interval_ranges]
+
+        # Create a bar chart for negative scores distribution
+        plt.bar(range(num_intervals), interval_counts, tick_label=[
+                f"{lower:.1f}-{upper:.1f}" for lower, upper in interval_ranges])
+        plt.xlabel("Negative Score Intervals")
+        plt.ylabel("Number of Comments")
+        plt.title("Distribution of Negative Scores")
+        plt.xticks(rotation=45, ha="right")
+
+        plt.tight_layout()
+        plt.show()
--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import pandas as pd
 from youtube_manager import youtube_manager
 from analysis import sentiment_analysis, analysis_displayer
 
-# YouTube Data API v3 API Key
-# Read the API key from key.txt
+# Load the YouTube Data API v3 API Key from key.txt
 with open("key.txt", "r") as key_file:
     API_KEY = key_file.read().strip()
 
 
+# Function to check if DataFrame has the required number of columns
 def valid_df(required_columns):
-    if (df.shape[1] < required_columns):
+    if df.shape[1] < required_columns:
         print("Cannot Perform this yet")
         return False
     return True
 
 
+# Function to save DataFrame to a CSV file
 def save_df():
     df.to_csv("saved_df.csv", index=False)
     print("DataFrame saved to 'saved_df.csv'.")
 
 
+# Function to read DataFrame from a CSV file
 def read_df():
     try:
         # Read the CSV file into a DataFrame
@@ -29,11 +31,15 @@ def read_df():
         return pd.DataFrame()
 
 
+# Load or initialize the DataFrame
 df = read_df()
+
+# Create instances of classes for YouTube management, sentiment analysis, and displaying analysis
 yt_manager = youtube_manager(API_KEY)
 sent_analyser = sentiment_analysis()
 displayer = analysis_displayer()
 
+# Main loop for user interaction
 while True:
     print(
         "Welcome to the project," +
@@ -50,22 +56,29 @@ while True:
         if user_input == 1:
             df = yt_manager.collect_comments()
             save_df()
+
         # Conduct Sentiment Analysis
         elif user_input == 2:
-            if (valid_df(required_columns=1)):
+            if valid_df(required_columns=1):
                 df = sent_analyser.analyse(df)
                 save_df()
+
         # Display Analysis
         elif user_input == 3:
-            if (valid_df(required_columns=2)):
-                displayer.display()
-         # Exit the console
+            if valid_df(required_columns=2):
+                displayer.display(df)
+                displayer.display_negative_graph(df)
+                displayer.display_positive_graph(df)
+
+        # Exit the console
         elif user_input == 0:
             print("Goodbye")
             break  # Exit the function and terminate the loop
+
         else:
             # User enters an integer that is not a valid option
             print("Invalid Input")
+
     except Exception as e:
         # Error handling when input is in an invalid format (not an integer)
         print(e)
